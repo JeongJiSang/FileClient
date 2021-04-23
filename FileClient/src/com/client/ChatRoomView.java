4d9a62416e96f0cd29026d667832528a4b1fdea3
Sang Chul <webtree12@gmail.com>
2021-04-23 22:18:44 +0900

    21/04/23 채팅방에서 파일전송 보내면 서버에서 msg로 받음. 양상철.

--- FileClient/src/com/client/ChatRoomView.java
+++ FileClient/src/com/client/ChatRoomView.java
@@ -1,11 +1,11 @@
 package com.client;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.awt.FileDialog;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.io.IOException;
 
 import javax.swing.JButton;
@@ -16,8 +16,6 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import com.common.Protocol;
-import com.file.ClientAddress;
-import com.file.FileSocket;
 
 public class ChatRoomView extends JFrame{
 	ActionHandler action = null;
@@ -26,17 +24,25 @@ public class ChatRoomView extends JFrame{
 
 	JPanel	jp_first = new JPanel();//채팅내용 보여주는 부분.
 	JPanel	jp_first_south = new JPanel();//채팅메세지 입력 부분.
-	JPanel  jp_second = new JPanel();//채팅방에 있는 유저 보여주기 & 나가기 버튼 부분.
+	JPanel  jp_second = new JPanel();//채팅방에 있는 유저 보여주기.
+	JPanel  jp_second_south = new JPanel();
 
 	JTextField jtf_msg = new JTextField(); 
 	JTextArea jta_display = new JTextArea();
+	JTextArea jta_user_display = new JTextArea();//채팅방 유저 아이디 부분.
 	JScrollPane jsp_display = new JScrollPane(jta_display,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED
 	        									, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+	JScrollPane jsp_user_display = new JScrollPane(jta_user_display,
+												JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED
+												,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
 	JButton jbtn_send  = new JButton("전송");
 	JButton jbtn_exit  = new JButton("나가기");
+	JButton jbtn_file  = new JButton("파일전송");
 	
-	   Font font = new Font("고딕체",Font.BOLD,15);	
+	FileDialog fd = new FileDialog(this);
+	
+	Font font = new Font("고딕체",Font.BOLD,15);	
 
 	public ChatRoomView(ClientSocket client, String roomName) {
 		this.client = client;
@@ -45,6 +51,14 @@ public class ChatRoomView extends JFrame{
 		initDisplay();
 	}
 	
+	
+	
+	public ChatRoomView() {
+		initDisplay();
+	}
+
+
+
 	private void initDisplay() {
 		
 		//채팅 메세지 보내기.
@@ -89,9 +103,35 @@ public class ChatRoomView extends JFrame{
 			}
 		});
 		
+		//파일전송.
+		jbtn_file.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("file!!");
+			    fd.setDirectory("..");
+			    fd.setVisible(true);
+			    String fileName = fd.getFile();
+			    String filePath = fd.getDirectory();
+			    System.out.println(fileName);
+			    System.out.println(filePath);
+			    try {
+					client.send(Protocol.sendFile,roomName, filePath, fileName, Protocol.myID);
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+			
+		});
+		
+		//show 채팅내용
 		jta_display.setFont(font);
 		jta_display.setEditable(false);
 		jta_display.setLineWrap(true);
+		
+		//show 채팅방유저 
+		jta_user_display.setFont(font);
+		jta_user_display.setEditable(false);
 
 		jp_first.setLayout(new BorderLayout());
 		jp_first.add("Center", jsp_display);
@@ -102,7 +142,13 @@ public class ChatRoomView extends JFrame{
 		jp_first_south.add("East",jbtn_send);
 		
 		jp_second.setLayout(new BorderLayout());
-		jp_second.add("South", jbtn_exit);
+		jp_second.add("Center", jsp_user_display);
+		jp_second.add("South", jp_second_south);
+		
+		jp_second_south.setLayout(new GridLayout(0,1,2,2));
+		jp_second_south.add(jbtn_file);
+		jp_second_south.add(jbtn_exit);
+		
 		
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		this.add("Center", jp_first);
@@ -110,6 +156,9 @@ public class ChatRoomView extends JFrame{
 		this.setBounds(1000, 200, 500, 600);
 		//jf.setSize(500, 600);
 		this.setVisible(true);
-		
+	}
+	
+	public static void main(String[] args) {
+		new ChatRoomView();
 	}
 }
