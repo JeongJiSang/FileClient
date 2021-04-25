88684eab0fdbe9381b57dc77284c1ad0cc09cacb
cjk3469 <edc3665@gmail.com>
2021-04-26 00:53:37 +0900

    4.26

--- FileClient/src/com/client/ActionHandler.java
+++ FileClient/src/com/client/ActionHandler.java
@@ -13,55 +13,58 @@ import javax.swing.JOptionPane;
 
 import com.common.Protocol;
 
-public class ActionHandler implements ActionListener, FocusListener, ItemListener{
+public class ActionHandler implements ActionListener, FocusListener, ItemListener {
 	private ClientSocket client = null;// 서버와 연결된 oos, ois가 상주하는 핵심 소켓클래스
-	
+
 	private LoginView logView = null;
 	private AddUserView addView = null;
 	private DefaultView defView = null;
 	private ChatRoomView chatView = null;
 	private CreateChattingView ccView = null;
 	private SelectFileView selectView = null;
-	
-	ActionHandler(){}
-	
+
+	ActionHandler() {
+	}
+
 	public void setInstance(LoginView logView, ClientSocket client) {
 		this.logView = logView;
 		this.client = client;
 	}
+
 	public void setInstance(AddUserView addView) {
 		this.addView = addView;
 	}
+
 	public void setInstance(DefaultView defView) {
 		this.defView = defView;
 	}
+
 	public void setInstance(ChatRoomView chatView) {
 		this.chatView = chatView;
 	}
+
 	public void setInstance(CreateChattingView ccView) {
 		this.ccView = ccView;
 	}
+
 	public void setInstance(SelectFileView selectView) {
 		this.selectView = selectView;
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent ae) {
 		Object obj = ae.getSource();
-	//로그인뷰 액션
+		// 로그인뷰 액션
 		try {
-			if(obj.equals(logView.jbtn_login)||obj.equals(logView.jtf_pw)) {
+			if (obj.equals(logView.jbtn_login) || obj.equals(logView.jtf_pw)) {
 				Protocol.myID = logView.jtf_id.getText();
-				client.send(Protocol.checkLogin
-							, logView.jtf_id.getText()
-							, logView.jtf_pw.getText());
-				
-			}else if(obj.equals(logView.jbtn_join)) {
+				client.send(Protocol.checkLogin, logView.jtf_id.getText(), logView.jtf_pw.getText());
+
+			} else if (obj.equals(logView.jbtn_join)) {
 				client.send(Protocol.addUserView);
-	//회원가입
+				// 회원가입
 			}
-//			
-//			  }else if(obj.equals(addView.jbtn_join) || obj.equals(addView.jtf_id)) {
+//				} else if (obj.equals(addView.jbtn_join) || obj.equals(addView.jtf_id)) {
 //				String new_id = addView.jtf_id.getText();
 //				String new_pw = addView.jtf_pw.getText();
 //				String new_name = addView.jtf_name.getText();
@@ -70,80 +73,80 @@ public class ActionHandler implements ActionListener, FocusListener, ItemListene
 //				} catch (IOException e1) {
 //					e1.printStackTrace();
 //				}
-//			 
+//
 //			}
-				
-	//기본화면
-			else if(obj.equals(defView.jbtn_chat)) {
+
+			// 기본화면
+			else if (obj.equals(defView.jbtn_chat)) {
 				client.send(Protocol.createRoomView, Protocol.myID);
-				
+
 			}
-	//로그아웃
-			else if(obj.equals(defView.jbtn_logout)) {
-				client.send(Protocol.logout,Protocol.myID);
+			// 로그아웃
+			else if (obj.equals(defView.jbtn_logout)) {
+				client.send(Protocol.logout, Protocol.myID);
 			}
-			
-	//유저선택화면
-			else if(obj.equals(ccView.jbtn_create)) {
+
+			// 유저선택화면
+			else if (obj.equals(ccView.jbtn_create)) {
 				String roomName = JOptionPane.showInputDialog("방 이름을 설정해주세요.");
 				try {
-					client.send(Protocol.createRoom,roomName
-							,Protocol.myID,ccView.selected_ID.toString());
+					client.send(Protocol.createRoom, roomName, Protocol.myID, ccView.selected_ID.toString());
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 				ccView.dispose();
 			}
-	//채팅화면
+			// 채팅화면
 			/*
-			else if(obj.equals(chatView.jbtn_send)) {
-				client.send(Protocol.sendMessage, chatView.jtf_msg.getText());
-			}*/
-	//중간입장
-			else if(obj.equals(defView.jbtn_enter)) {
-				//디티엠 선택한 방이름 setText하기
-				
-				
+			 * else if(obj.equals(chatView.jbtn_send)) { client.send(Protocol.sendMessage,
+			 * chatView.jtf_msg.getText()); }
+			 */
+			// 중간입장
+			else if (obj.equals(defView.jbtn_enter)) {			
 				
 			}
-	//파일전송화면
-			
-			
+			// 파일전송화면
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	public void getroomName() {
+		int[] selectRoom = defView.jtb_room.getSelectedRows();
+		for(int room: selectRoom) {
+			String roomName = defView.jtb_room.getValueAt(room,0).toString();
+			
+		}		
+	}
 	@Override
 	public void focusGained(FocusEvent fe) {
 		Object obj = fe.getSource();
-		
-		if(obj == addView.jtf_id ) {
+
+		if (obj == addView.jtf_id) {
 			addView.jtf_id.setText("");
-		}
-		else if(obj == addView.jtf_pw) {
+		} else if (obj == addView.jtf_pw) {
 			addView.jtf_pw.setText("");
-		}
-		else if(obj == addView.jtf_name) {
+		} else if (obj == addView.jtf_name) {
 			addView.jtf_name.setText("");
 		}
-		
+
 	}
 
 	@Override
 	public void focusLost(FocusEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	@Override
 	public void itemStateChanged(ItemEvent ie) {
 		Object obj = ie.getSource();
-		if(ie.getStateChange() == ie.SELECTED) {
-			ccView.selected_ID.add(((JCheckBox) ie.getSource()).getText()); //체크박스의 값 들어가야함.
+		if (ie.getStateChange() == ie.SELECTED) {
+			ccView.selected_ID.add(((JCheckBox) ie.getSource()).getText()); // 체크박스의 값 들어가야함.
 		}
-		
-		else if(ie.getStateChange() == ie.DESELECTED) {
-			ccView.selected_ID.remove(((JCheckBox) ie.getSource()).getText()); //체크박스의 값 들어가야함.
+
+		else if (ie.getStateChange() == ie.DESELECTED) {
+			ccView.selected_ID.remove(((JCheckBox) ie.getSource()).getText()); // 체크박스의 값 들어가야함.
 		}
 	}
 }
